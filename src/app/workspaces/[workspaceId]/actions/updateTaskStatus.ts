25049a95e59a8c57944637f2fd951edd4b111f9b
eugene <eugene97@connect.hku.hk>
2025-09-27 16:47:00 +0900

    refactor: add authentication to updateTaskStatus

--- src/app/workspaces/[workspaceId]/actions/updateTaskStatus.ts
+++ src/app/workspaces/[workspaceId]/actions/updateTaskStatus.ts
@@ -2,12 +2,24 @@
 import { prisma } from "@/utils/prismaClient";
 import { changeTaskStatus } from "@prisma-generated/sql";
 import { revalidatePath } from "next/cache";
+import { auth } from "@clerk/nextjs/server";
 
 export async function updateTaskStatus(
   taskId: number,
   statusId: number,
   workspaceId: number,
 ) {
+  const { isAuthenticated, userId } = await auth();
+
+  if (!isAuthenticated || !userId) throw new Error("Unauthorized User");
+
+  await prisma.userWorkspace.findFirstOrThrow({
+    where: {
+      userId,
+      workspaceId,
+    },
+  });
+
   await prisma.$queryRawTyped(changeTaskStatus(taskId, workspaceId, statusId));
 
   revalidatePath(`/workspaces/${workspaceId}`);
