29e0e78cd0fbbaeed3ab4dfb59697d040e902b05
eugene <eugene97@connect.hku.hk>
2025-10-09 22:30:31 +0900

    refactor: extract WorkspaceView

---
+++ src/app/workspaces/[workspaceId]/components/WorkspaceView.tsx
@@ -0,0 +1,98 @@
+import { prisma } from "@/utils/prismaClient";
+
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { TaskCard } from "./TaskCard";
+import { updateTaskStatus } from "../actions/updateTaskStatus";
+import { CreateTaskDialog } from "../components/CreateTaskDialog";
+
+type Props = {
+  userId: string;
+  workspaceId: number;
+  taskId?: number;
+};
+
+export async function WorkspaceView({ userId, workspaceId, taskId }: Props) {
+  const workspace = await prisma.userWorkspace.findFirst({
+    where: {
+      workspaceId,
+      userId,
+    },
+    include: {
+      workspace: true,
+    },
+  });
+
+  if (!workspace) return <div>workspace does not exist</div>;
+
+  const statuses = await prisma.status.findMany();
+
+  const tasks = await prisma.task.findMany({
+    where: { workspaceId },
+    include: {
+      status: true,
+    },
+    orderBy: [{ sortOrder: "desc" }, { updatedAt: "desc" }],
+  });
+
+  if (!!taskId && !tasks.some(({ id }) => id === taskId)) {
+    return <div>task does not exist</div>;
+  }
+
+  const tasksByStatus = statuses.map(({ id, name }) => {
+    const tasksOfStatus: typeof tasks = [];
+    tasks.forEach((task) => {
+      if (task.status.id === id) {
+        tasksOfStatus.push(task);
+      }
+    });
+
+    return {
+      id,
+      name,
+      tasks: tasksOfStatus,
+    };
+  });
+
+  return (
+    <div className="flex flex-1 flex-col px-4 sm:px-6 lg:px-12 gap-6 lg:gap-8">
+      <div className="text-2xl font-bold text-center sm:text-left">
+        {workspace.workspace.name}
+      </div>
+      <CreateTaskDialog workspaceId={workspaceId} />
+      <div className="flex flex-1 flex-col sm:flex-row gap-4 lg:gap-6">
+        {tasksByStatus.map(({ id: statusId, name, tasks }, statusIdx) => (
+          <Card key={statusId} className="w-full sm:flex-1">
+            <CardHeader className="pb-4">
+              <CardTitle className="text-lg">{name}</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-2 max-h-96 lg:max-h-none overflow-y-auto lg:overflow-visible">
+              {tasks.map(({ id: taskId, title, description }) => {
+                const nextStatusId =
+                  statusIdx < statuses.length - 1
+                    ? statuses[statusIdx + 1].id
+                    : null;
+                const nextAction = nextStatusId
+                  ? {
+                      label: "NEXT",
+                      taskId,
+                      nextStatusId,
+                      workspaceId: workspaceId,
+                      onNextActionClick: updateTaskStatus,
+                    }
+                  : undefined;
+                return (
+                  <TaskCard
+                    key={taskId}
+                    title={title}
+                    description={description}
+                    nextAction={nextAction}
+                  />
+                );
+              })}
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/app/workspaces/[workspaceId]/page.tsx
+++ src/app/workspaces/[workspaceId]/page.tsx
@@ -1,10 +1,6 @@
 import { auth } from "@clerk/nextjs/server";
-import { prisma } from "@/utils/prismaClient";
 
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { TaskCard } from "./components/TaskCard";
-import { updateTaskStatus } from "./actions/updateTaskStatus";
-import { CreateTaskDialog } from "./components/CreateTaskDialog";
+import { WorkspaceView } from "./components/WorkspaceView";
 
 type PageParams = {
   workspaceId: string;
@@ -23,83 +19,5 @@ export default async function Page({
 
   const workspaceIdNumber = Number(workspaceId);
 
-  const workspace = await prisma.userWorkspace.findFirst({
-    where: {
-      workspaceId: workspaceIdNumber,
-      userId,
-    },
-    include: {
-      workspace: true,
-    },
-  });
-
-  if (!workspace) return <div>workspace does not exist</div>;
-
-  const statuses = await prisma.status.findMany();
-
-  const tasks = await prisma.task.findMany({
-    where: { workspaceId: workspaceIdNumber },
-    include: {
-      status: true,
-    },
-    orderBy: [{ sortOrder: "desc" }, { updatedAt: "desc" }],
-  });
-
-  const tasksByStatus = statuses.map(({ id, name }) => {
-    const tasksOfStatus: typeof tasks = [];
-    tasks.forEach((task) => {
-      if (task.status.id === id) {
-        tasksOfStatus.push(task);
-      }
-    });
-
-    return {
-      id,
-      name,
-      tasks: tasksOfStatus,
-    };
-  });
-
-  return (
-    <div className="flex flex-1 flex-col px-4 sm:px-6 lg:px-12 gap-6 lg:gap-8">
-      <div className="text-2xl font-bold text-center sm:text-left">
-        {workspace.workspace.name}
-      </div>
-      <CreateTaskDialog workspaceId={workspaceIdNumber} />
-      <div className="flex flex-1 flex-col sm:flex-row gap-4 lg:gap-6">
-        {tasksByStatus.map(({ id: statusId, name, tasks }, statusIdx) => (
-          <Card key={statusId} className="w-full sm:flex-1">
-            <CardHeader className="pb-4">
-              <CardTitle className="text-lg">{name}</CardTitle>
-            </CardHeader>
-            <CardContent className="flex flex-col gap-2 max-h-96 lg:max-h-none overflow-y-auto lg:overflow-visible">
-              {tasks.map(({ id: taskId, title, description }) => {
-                const nextStatusId =
-                  statusIdx < statuses.length - 1
-                    ? statuses[statusIdx + 1].id
-                    : null;
-                const nextAction = nextStatusId
-                  ? {
-                      label: "NEXT",
-                      taskId,
-                      nextStatusId,
-                      workspaceId: workspaceIdNumber,
-                      onNextActionClick: updateTaskStatus,
-                    }
-                  : undefined;
-                return (
-                  <TaskCard
-                    key={taskId}
-                    title={title}
-                    description={description}
-                    nextAction={nextAction}
-                  />
-                );
-              })}
-            </CardContent>
-          </Card>
-        ))}
-      </div>
-    </div>
-  );
+  return <WorkspaceView workspaceId={workspaceIdNumber} userId={userId} />;
 }
